f72446cf8d59090cbc5700bc1e7e469ba47b122e
flgisimone <florestagiuliosimone@gmail.com>
2023-01-28 19:54:40 +0100

    working media query

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from 'react';
+import Header from './components/Header/Header';
 import StoriesList from './components/StoriesList/StoriesList';
+import MQSuggestFriendList from './components/MQSuggestFriendList/MQSuggestFriendList';
 import NewPost from './components/NewPost/NewPost';
 import TrendPostList from './components/TrendPostList/TrendPostList';
 import PostList from './components/PostList/PostList';
@@ -7,6 +9,7 @@ import SuggestFriendList from './components/SuggestFriendList/SuggestFriendList'
 import FriendList from './components/FriendList/FriendList';
 import Login from './components/Login/Login';
 import Account from './components/Account/Account';
+import MQNavBar from './components/MQNavBar/MQNavBar';
 import Footer from './components/Footer/Footer';
 import styles from './App.module.scss';
 
@@ -24,9 +27,17 @@ function App() {
 
   return user ? (
     <div className={styles.App}>
+
+      <section className={styles.containerHeader}>
+        <Header />
+      </section>
       <section className={styles.containerStoriesList}>
         <StoriesList />
       </section>
+      <section className={styles.containerMQSuggestFriendList}>
+        <MQSuggestFriendList />
+      </section>
+      
 
       <section className={styles.main}>
         <div className={styles.leftBar}>
@@ -45,6 +56,10 @@ function App() {
           <SuggestFriendList />
           <Footer />
         </div>
+      </section>
+
+      <section className={styles.containerMQNavBar}>
+        <MQNavBar />
       </section>
 
     </div>) : (<>
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import Account from "../../components/Account/Account"
+import styles from "./Header.module.scss"
+
+const Header = () => {
+  return (
+    <div className={styles.Header}>
+        <div className={styles.containerHeader}>
+            <div className={styles.nameSocial}>
+              <h1>Memories</h1>
+              <span>Share Your Moment</span>
+            </div>
+            <Account />
+        </div>
+    </div>
+  )
+}
+
+export default Header
